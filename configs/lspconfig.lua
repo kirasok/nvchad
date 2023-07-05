7e92ae060ac27c51711d667c483f98aa45c0c473
Kira Sokolova <Kyra256@proton.me>
2023-07-06 02:15:02 +0300

    feat(haskell): lsp and formatter

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,14 +1,14 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "rust_analyzer", "bashls", "taplo", "yamlls", "pyright" }
+local servers = { "rust_analyzer", "bashls", "taplo", "yamlls", "pyright", "hls" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,37 +1,38 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 
 local sources = {
-  formatting.stylua,
-  formatting.taplo,
-  formatting.yamlfmt,
-  formatting.ruff,
-  formatting.shfmt.with { extra_filetypes = { "zsh" } },
-  formatting.deno_fmt.with { extra_args = { "--line-width", "9999" } },
-  lint.deno_lint,
-  lint.zsh,
-  lint.actionlint,
-  lint.shellcheck,
-  lint.ruff,
+	formatting.stylua,
+	formatting.taplo,
+	formatting.yamlfmt,
+	formatting.ruff,
+	formatting.shfmt.with({ extra_filetypes = { "zsh" } }),
+	formatting.deno_fmt.with({ extra_args = { "--line-width", "9999" } }),
+	formatting.stylish_haskell,
+	lint.deno_lint,
+	lint.zsh,
+	lint.actionlint,
+	lint.shellcheck,
+	lint.ruff,
 }
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-null_ls.setup {
-  debug = true,
-  sources = sources,
-  -- you can reuse a shared lspconfig on_attach callback here
-  on_attach = function(client, bufnr)
-    if client.supports_method "textDocument/formatting" then
-      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format { async = false }
-        end,
-      })
-    end
-  end,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
+		end
+	end,
+})
