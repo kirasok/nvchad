d02405616267daa3b187e4200a09431e5ebab053
Kira Sokolova <Kyra256@proton.me>
2023-07-10 09:48:50 +0300

    feat(plugins): lsp_lines

--- plugins.lua
+++ plugins.lua
@@ -353,6 +353,18 @@ local plugins = {
 		"elkowar/yuck.vim",
 		ft = "yuck",
 	},
+
+	{
+		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+		event = "LspAttach",
+		config = function()
+			require("lsp_lines").setup()
+			-- Disable virtual_text since it's redundant due to lsp_lines.
+			vim.diagnostic.config({
+				virtual_text = false,
+			})
+		end,
+	},
 }
 
 return plugins
