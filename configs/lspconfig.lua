acfc5734614f4c1905603fae1cf5c9d54770c1d3
Kira Sokolova <Kyra256@proton.me>
2023-07-19 10:57:53 +0300

    feat(lspconfig): enable lsp formatting

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,5 +1,23 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
+local nvchad_on_attach = require("plugins.configs.lspconfig").on_attach
+local nvchad_capabilities = require("plugins.configs.lspconfig").capabilities
+
+local on_attach = function(client, bufnr)
+	nvchad_on_attach(client, bufnr)
+	client.server_capabilities.documentFormattingProvider = true
+	client.server_capabilities.documentRangeFormattingProvider = true
+	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
+	end
+end
+local capabilities = nvchad_capabilities
 
 local lspconfig = require("lspconfig")
 
--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -7,31 +7,13 @@ local sources = {
 	formatting.stylua,
 	formatting.taplo,
 	formatting.yamlfmt,
-	formatting.black,
-	formatting.gofumpt,
 	formatting.shfmt.with({ extra_filetypes = { "zsh" } }),
 	formatting.deno_fmt.with({ extra_args = { "--line-width", "9999" } }),
-	formatting.stylish_haskell,
 	lint.deno_lint,
 	lint.zsh,
 	lint.actionlint,
 }
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
-	debug = true,
 	sources = sources,
-	-- you can reuse a shared lspconfig on_attach callback here
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ async = false })
-				end,
-			})
-		end
-	end,
 })
