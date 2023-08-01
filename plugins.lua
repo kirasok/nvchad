8a3ba9141a5f40894a6aaa68329017c620c2721b
Kira Sokolova <Kyra256@proton.me>
2023-08-01 15:44:28 +0300

    feat(nvim-tree): don't sort folder first

--- plugins.lua
+++ plugins.lua
@@ -30,6 +30,26 @@ local plugins = {
 	},
 
 	{
+		"nvim-tree/nvim-tree.lua",
+		cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+		init = function()
+			require("core.utils").load_mappings("nvimtree")
+		end,
+		opts = function()
+			return require("plugins.configs.nvimtree")
+		end,
+		config = function(_, opts)
+			dofile(vim.g.base46_cache .. "nvimtree")
+			require("nvim-tree").setup(opts)
+			require("nvim-tree").setup({
+				sort = {
+					folders_first = false,
+				},
+			})
+		end,
+	},
+
+	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
 			"jose-elias-alvarez/null-ls.nvim",
