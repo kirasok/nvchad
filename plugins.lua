fba8c12cb7cad807140c040d6fbcab4790202b4b
Kira Sokolova <Kyra256@proton.me>
2023-10-02 21:45:51 +0300

    feat(plugins): tsnode-marker.nvim

--- plugins.lua
+++ plugins.lua
@@ -349,6 +349,23 @@ local plugins = {
 		cmd = { "SymbolsOutline", "SymbolsOutlineOpen", "SymbolsOutlineClose" },
 		config = true,
 	},
+
+	{
+		"atusy/tsnode-marker.nvim", -- TODO: fix dark color of codeblock marks and language name
+		lazy = true,
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("tsnode-marker-markdown", {}),
+				pattern = "markdown",
+				callback = function(ctx)
+					require("tsnode-marker").set_automark(ctx.buf, {
+						target = { "code_fence_content" }, -- list of target node types
+						hl_group = "CursorLine", -- highlight group
+					})
+				end,
+			})
+		end,
+	},
 }
 
 return plugins
