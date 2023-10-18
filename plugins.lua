ff11c704ba2d029792d96c5b231b3d3c9cd1fa77
Kira Sokolova <Kyra256@proton.me>
2023-10-19 00:53:13 +0300

    feat(plugins): nvim-spider

--- plugins.lua
+++ plugins.lua
@@ -390,6 +390,22 @@ local plugins = {
 		lazy = "leetcode.nvim" ~= vim.fn.argv()[1],
 		config = true,
 	},
+
+	{
+		"chrisgrieser/nvim-spider",
+		event = "BufEnter",
+		config = function(_, opts)
+			vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+			vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+			vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
+			vim.keymap.set(
+				{ "n", "o", "x" },
+				"ge",
+				"<cmd>lua require('spider').motion('ge')<CR>",
+				{ desc = "Spider-ge" }
+			)
+		end,
+	},
 }
 
 return plugins
