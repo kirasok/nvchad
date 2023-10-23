25ba444f73b3c513095ebc75d191c5c68bd19201
Kira Sokolova <Kyra256@proton.me>
2023-10-23 22:14:10 +0300

    remove(plugin): tsnode-marker.nvim

--- plugins.lua
+++ plugins.lua
@@ -351,23 +351,6 @@ local plugins = {
 	},
 
 	{
-		"atusy/tsnode-marker.nvim", -- TODO: fix dark color of codeblock marks and language name
-		lazy = true,
-		init = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				group = vim.api.nvim_create_augroup("tsnode-marker-markdown", {}),
-				pattern = "markdown",
-				callback = function(ctx)
-					require("tsnode-marker").set_automark(ctx.buf, {
-						target = { "code_fence_content" }, -- list of target node types
-						hl_group = "CursorLine", -- highlight group
-					})
-				end,
-			})
-		end,
-	},
-
-	{
 		"calops/hmts.nvim",
 		ft = "nix",
 		version = "*",
