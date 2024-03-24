d2016adb92112ce5f8d0dc76200b4a4e3aa9d37b
Kira Sokolova <Kyra256@proton.me>
2024-03-25 01:02:41 +0300

    plugins: Wansmer/symbol-usage.nvim

--- plugins.lua
+++ plugins.lua
@@ -499,6 +499,12 @@ local plugins = {
 			},
 		},
 	},
+
+	{
+		"Wansmer/symbol-usage.nvim",
+		event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. WARN: On 0.10 use 'LspAttach'
+		config = true,
+	},
 }
 
 return plugins
