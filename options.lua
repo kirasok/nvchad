f07c4ed3b5e5f7d07c66f40c9f0658b407b28a45
Kira Sokolova <Kyra256@proton.me>
2024-05-08 05:54:35 +0300

    feat: enable python provider

--- options.lua
+++ options.lua
@@ -6,3 +6,13 @@ vim.opt.conceallevel = 2
 vim.opt.spell = true
 vim.opt.spelllang = { "en", "ru" }
 vim.opt.scrolloff = 3
+
+-- enable python provider
+local enable_providers = {
+	"python3_provider",
+}
+
+for _, plugin in pairs(enable_providers) do
+	vim.g["loaded_" .. plugin] = nil
+	vim.cmd("runtime " .. plugin)
+end
