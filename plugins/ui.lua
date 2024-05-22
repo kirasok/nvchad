75a1eafbf3c86533d0957d4a9955f3ff9a938f1d
Kira Sokolova <Kyra256@proton.me>
2024-05-22 18:16:59 +0300

    Revert "plugins: RRethy/vim-illuminate"
    This reverts commit 5741829326bcd18780bc564fd8062d7978679c43.

--- plugins/ui.lua
+++ plugins/ui.lua
@@ -175,42 +175,6 @@ local plugins = {
 			disabled_filetypes = { "quickfix" },
 		},
 	},
-
-	{
-		"RRethy/vim-illuminate",
-		event = "BufEnter",
-		opts = {
-			-- providers: provider used to get references in the buffer, ordered by priority
-			providers = {
-				"lsp",
-				"treesitter",
-				"regex",
-			},
-			delay = 100,
-			filetypes_denylist = {
-				"dirvish",
-				"fugitive",
-				"alpha",
-				"NvimTree",
-				"packer",
-				"neogitstatus",
-				"Trouble",
-				"lir",
-				"Outline",
-				"spectre_panel",
-				"toggleterm",
-				"DressingSelect",
-				"TelescopePrompt",
-				"aerial",
-				"Empty",
-			},
-			under_cursor = true,
-			max_file_lines = nil,
-		},
-		config = function(_, opts)
-			require("illuminate").configure(opts)
-		end,
-	},
 }
 
 return plugins
