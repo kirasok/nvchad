d8c7e34692164294c286a4538495c4ad9decf2db
Kira Sokolova <Kyra256@proton.me>
2024-09-22 08:12:44 +0300

    plugins(MeanderingProgrammer/render-markdown.nvim): disable latex and sings

--- plugins/documents.lua
+++ plugins/documents.lua
@@ -4,7 +4,10 @@ local plugins = {
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		ft = "markdown",
-		opts = {},
+		opts = {
+			sign = { enabled = false },
+			latex = { enabled = false },
+		},
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
 	},
 
