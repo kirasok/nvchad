86fa197035ad6f0c24af5a11fef3c10c23fb71c9
Kira Sokolova <Kyra256@proton.me>
2024-10-06 10:45:58 +0300

    refactor: chadrc

--- chadrc.lua
+++ chadrc.lua
@@ -1,38 +1,40 @@
-local M = {}
-
 -- Path to overriding theme and highlights files
 local highlights = require("highlights")
 
-M.ui = {
-	theme = "rosepine",
-	theme_toggle = { "kanawaga", "papercolor-light" },
-	transparency = true,
-	lsp_semantic_tokens = true,
+local options = {
+	base46 = {
+		hl_override = highlights.override,
+		hl_add = highlights.add,
+		integrations = {},
+		changed_themes = {},
+		transparency = true,
+		theme = "rosepine",
+		theme_toggle = { "kanawaga", "papercolor-light" },
+	},
 
-	extended_integrations = { "dap" },
+	ui = {
+		cmp = {
+			style = "atom", -- default/flat_light/flat_dark/atom/atom_colored
+		},
 
-	hl_override = highlights.override,
-	hl_add = highlights.add,
-
-	statusline = {
-		theme = "vscode_colored",
+		statusline = {
+			theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+		},
 	},
 
 	nvdash = {
 		load_on_startup = true,
-		buttons = {
-			{ "  Find File", "Spc f f", "Telescope find_files" },
-			{ "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
-			{ "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
-			{ "  Bookmarks", "Spc f m", "Telescope marks" },
-			{ "  Mappings", "Spc i h", "NvCheatsheet" },
-		},
+	},
+
+	lsp = { signature = true },
+
+	colorify = {
+		enabled = true,
+		mode = "virtual", -- fg, bg, virtual
+		virt_text = "󱓻 ",
+		highlight = { hex = true, lspvars = true },
 	},
 }
 
-M.plugins = "plugins"
-
--- check core.mappings for table structure
-M.mappings = require("mappings")
-
-return M
+local status, chadrc = pcall(require, "chadrc")
+return vim.tbl_deep_extend("force", options, status and chadrc or {})
