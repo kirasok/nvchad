354d571d318e9b191a42914979948379a57e0d35
Kira Sokolova <Kyra256@proton.me>
2024-10-06 10:46:59 +0300

    refactor: don't load nvchad mappings

--- mappings/init.lua
+++ mappings/init.lua
@@ -1,28 +1,64 @@
-require("nvchad.mappings")
-
 local map = vim.keymap.set
-local nomap = vim.keymap.del
 
 map("i", "<C-l>", "<Esc>[s1z=`]a", { desc = "spell fix last mistake" })
 map("n", "j", "gj", { desc = "move 1 line down" })
 map("n", "k", "gk", { desc = "move 1 line up" })
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
--- lspconfig
-nomap("n", "<leader>fm")
-nomap("n", "<leader>ds")
+map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
+map("i", "<C-e>", "<End>", { desc = "move end of line" })
+map("i", "<C-h>", "<Left>", { desc = "move left" })
+map("i", "<C-l>", "<Right>", { desc = "move right" })
+map("i", "<C-j>", "<Down>", { desc = "move down" })
+map("i", "<C-k>", "<Up>", { desc = "move up" })
 
--- telescope
-nomap("n", "<leader>cm")
-nomap("n", "<leader>gt")
-nomap("n", "<leader>pt")
-nomap("n", "<leader>th")
+map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
+map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
 
-nomap("n", "<leader>cc")
-nomap("n", "<leader>rn")
-nomap("n", "<leader>wk")
-nomap("n", "<leader>wK")
-nomap("n", "<leader>ch")
-nomap("n", "<leader>ma")
-nomap("n", "<C-n>")
-nomap("n", "<leader>n")
+map("n", "<Esc>", "<cmd>noh<CR>", { desc = "General Clear highlights" })
+
+map("n", "<C-s>", "<cmd>w<CR>", { desc = "General Save file" })
+map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "General Copy whole file" })
+
+map("n", "<tab>", function()
+	require("nvchad.tabufline").next()
+end, { desc = "buffer goto next" })
+
+map("n", "<S-tab>", function()
+	require("nvchad.tabufline").prev()
+end, { desc = "buffer goto prev" })
+
+map("n", "<leader>x", function()
+	require("nvchad.tabufline").close_buffer()
+end, { desc = "buffer close" })
+
+-- Comment
+map("n", "<leader>/", "gcc", { desc = "Toggle Comment", remap = true })
+map("v", "<leader>/", "gc", { desc = "Toggle comment", remap = true })
+
+-- terminal
+map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
+
+-- new terminals
+map("n", "<leader>h", function()
+	require("nvchad.term").new({ pos = "sp" })
+end, { desc = "terminal new horizontal term" })
+
+map("n", "<leader>v", function()
+	require("nvchad.term").new({ pos = "vsp" })
+end, { desc = "terminal new vertical window" })
+
+-- toggleable
+map({ "n", "t" }, "<A-v>", function()
+	require("nvchad.term").toggle({ pos = "vsp", id = "vtoggleTerm" })
+end, { desc = "terminal toggleable vertical term" })
+
+map({ "n", "t" }, "<A-h>", function()
+	require("nvchad.term").toggle({ pos = "sp", id = "htoggleTerm" })
+end, { desc = "terminal toggleable horizontal term" })
+
+map({ "n", "t" }, "<A-i>", function()
+	require("nvchad.term").toggle({ pos = "float", id = "floatTerm" })
+end, { desc = "terminal toggle floating term" })
