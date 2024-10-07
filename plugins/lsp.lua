625d5b9b067e39fb44d7d557c692d4b99adab5c7
Kira Sokolova <Kyra256@proton.me>
2024-10-07 09:11:26 +0300

    plugins(Wansmer/symbol-usage.nvim): pretty reference display

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -11,7 +11,7 @@ local linters = {
 }
 
 local formatters = {
-	lua = { "stylua" },
+	-- lua = { "stylua" },
 	toml = { "taplo" },
 	yaml = { "yamlfmt" },
 	sh = { "shfmt" },
@@ -132,18 +132,18 @@ local plugins = {
 				},
 			})
 
-			lspconfig.lua_ls.setup({
-				settings = {
-					format = {
-						enable = false,
-					},
-					Lua = {
-						completion = {
-							callSnippet = "Replace",
-						},
-					},
-				},
-			})
+			-- lspconfig.lua_ls.setup({
+			-- 	settings = {
+			-- 		format = {
+			-- 			enable = false,
+			-- 		},
+			-- 		Lua = {
+			-- 			completion = {
+			-- 				callSnippet = "Replace",
+			-- 			},
+			-- 		},
+			-- 	},
+			-- })
 		end,
 	},
 
@@ -249,6 +249,74 @@ local plugins = {
 	{
 		"Wansmer/symbol-usage.nvim",
 		event = "LspAttach",
+		config = function(_, _)
+			local function h(name)
+				return vim.api.nvim_get_hl(0, { name = name })
+			end
+
+			-- hl-groups can have any name
+			vim.api.nvim_set_hl(0, "SymbolUsageRounding", { fg = h("Visual").bg, italic = true })
+			vim.api.nvim_set_hl(0, "SymbolUsageContent", { bg = h("Visual").bg, fg = h("Visual").fg, italic = true })
+			vim.api.nvim_set_hl(0, "SymbolUsageRef", { fg = h("@function").fg, bg = h("Visual").bg, italic = true })
+			vim.api.nvim_set_hl(0, "SymbolUsageDef", { fg = h("@type").fg, bg = h("Visual").bg, italic = true })
+			vim.api.nvim_set_hl(0, "SymbolUsageImpl", { fg = h("@keyword").fg, bg = h("Visual").bg, italic = true })
+
+			local function text_format(symbol)
+				local res = {}
+
+				local round_start = { "", "SymbolUsageRounding" }
+				local round_end = { "", "SymbolUsageRounding" }
+
+				-- Indicator that shows if there are any other symbols in the same line
+				local stacked_functions_content = symbol.stacked_count > 0 and ("+%s"):format(symbol.stacked_count)
+					or ""
+
+				if symbol.references then
+					local usage = symbol.references <= 1 and "usage" or "usages"
+					local num = symbol.references == 0 and "no" or symbol.references
+					table.insert(res, round_start)
+					table.insert(res, { "󰌹 ", "SymbolUsageRef" })
+					table.insert(res, { ("%s %s"):format(num, usage), "SymbolUsageContent" })
+					table.insert(res, round_end)
+				end
+
+				if symbol.definition then
+					if #res > 0 then
+						table.insert(res, { " ", "NonText" })
+					end
+					table.insert(res, round_start)
+					table.insert(res, { "󰳽 ", "SymbolUsageDef" })
+					table.insert(res, { symbol.definition .. " defs", "SymbolUsageContent" })
+					table.insert(res, round_end)
+				end
+
+				if symbol.implementation then
+					if #res > 0 then
+						table.insert(res, { " ", "NonText" })
+					end
+					table.insert(res, round_start)
+					table.insert(res, { "󰡱 ", "SymbolUsageImpl" })
+					table.insert(res, { symbol.implementation .. " impls", "SymbolUsageContent" })
+					table.insert(res, round_end)
+				end
+
+				if stacked_functions_content ~= "" then
+					if #res > 0 then
+						table.insert(res, { " ", "NonText" })
+					end
+					table.insert(res, round_start)
+					table.insert(res, { " ", "SymbolUsageImpl" })
+					table.insert(res, { stacked_functions_content, "SymbolUsageContent" })
+					table.insert(res, round_end)
+				end
+
+				return res
+			end
+
+			require("symbol-usage").setup({
+				text_format = text_format,
+			})
+		end,
 	},
 }
 
