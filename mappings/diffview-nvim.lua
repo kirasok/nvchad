66f0b4bc9bf3ec8e55849de654577ab9171b318f
Kira Sokolova <Kyra256@proton.me>
2024-10-09 15:12:44 +0300

    feat(mappings): add diffview for file history

--- mappings/diffview-nvim.lua
+++ mappings/diffview-nvim.lua
@@ -34,4 +34,16 @@ return {
 		end,
 		desc = "Diff commit",
 	},
+	{
+		"<leader>gl",
+		function()
+			local view = require("diffview.lib").get_current_view()
+			if view then
+				vim.cmd.DiffviewClose()
+			else
+				vim.cmd.DiffviewFileHistory()
+			end
+		end,
+		desc = "File history",
+	},
 }
