3ffe2c5a54100952b3c3ff418e0e3b2cb7fba0e4
Kira Sokolova <Kyra256@proton.me>
2024-10-17 07:14:01 +0300

    plugins: jiaoshijie/undotree

--- plugins/ui.lua
+++ plugins/ui.lua
@@ -183,6 +183,15 @@ local plugins = {
 			disabled_filetypes = { "quickfix" },
 		},
 	},
+
+	{
+		"jiaoshijie/undotree",
+		dependencies = "nvim-lua/plenary.nvim",
+		config = true,
+		keys = { -- load the plugin only when using it's keybinding:
+			{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>", desc = "undotree toggle" },
+		},
+	},
 }
 
 return plugins
