91bf92d8415737742304da40c523fc0bf6361b9f
Kira Sokolova <Kyra256@proton.me>
2024-10-20 15:50:42 +0300

    feat(lua_ls): set libraries in .luarc instead of global settings

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -142,17 +142,6 @@ local plugins = {
 						completion = {
 							callSnippet = "Replace",
 						},
-						workspace = {
-							library = {
-								vim.fn.expand("$VIMRUNTIME/lua"),
-								vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
-								vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
-								vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
-								"${3rd}/luv/library",
-							},
-							maxPreload = 100000,
-							preloadFileSize = 10000,
-						},
 					},
 				},
 			})
