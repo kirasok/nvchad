1f700a14f10c95fb40511ae99a20548dd376fc9e
Kira Sokolova <Kyra256@proton.me>
2024-11-14 19:51:09 +0300

    refactor(treesitter): move into separate file

--- plugins/core.lua
+++ plugins/core.lua
@@ -1,27 +1,6 @@
 ---@type NvPluginSpec[]
 local plugins = {
 	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = require("configs.nvim-treesitter"),
-	},
-
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-		},
-		event = "BufRead",
-	},
-
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-		},
-		event = "BufRead",
-	},
-
-	{
 		"kevinhwang91/nvim-ufo",
 		dependencies = "kevinhwang91/promise-async",
 		event = "BufEnter",
---
+++ plugins/nvim-treesitter.lua
@@ -0,0 +1,23 @@
+---@type NvPluginSpec
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = require("configs.nvim-treesitter"),
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		event = "BufRead",
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		event = "BufRead",
+	},
+}
