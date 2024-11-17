17ab2b82c74a1996f1a96b6eecedf2d6d5f44949
Kira Sokolova <Kyra256@proton.me>
2024-11-17 06:15:11 +0300

    refactor(telescope): remove redundant description

--- mappings/telescope.lua
+++ mappings/telescope.lua
@@ -3,16 +3,16 @@ require("which-key").add({
 })
 return {
 	telescope = {
-		{ "<leader>fw", "<cmd>Telescope live_grep<CR>", desc = "telescope live grep" },
-		{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "telescope find buffers" },
-		{ "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "telescope help page" },
-		{ "<leader>fo", "<cmd>Telescope oldfiles<CR>", desc = "telescope find oldfiles" },
-		{ "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "telescope find in current buffer" },
-		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "telescope find files" },
+		{ "<leader>fw", "<cmd>Telescope live_grep<CR>", desc = "live grep" },
+		{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "find buffers" },
+		{ "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "help page" },
+		{ "<leader>fo", "<cmd>Telescope oldfiles<CR>", desc = "find oldfiles" },
+		{ "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "find in current buffer" },
+		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "find files" },
 		{
 			"<leader>fa",
 			"<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
-			desc = "telescope find all files",
+			desc = "find all files",
 		},
 	},
 	text_case = {
@@ -20,7 +20,7 @@ return {
 			"<space>fc",
 			"<cmd>TextCaseOpenTelescope<CR>",
 			mode = { "n", "x" },
-			desc = "telescope convert text case",
+			desc = "convert text case",
 		},
 	},
 	direcotory = {
@@ -31,12 +31,12 @@ return {
 					feature = "live_grep", -- "find_files"|"grep_string"|"live_grep"
 				})
 			end,
-			desc = "telescope select directory for Live Grep",
+			desc = "select directory for Live Grep",
 		},
 		{
 			"<Leader>fe",
 			"<CMD>Telescope directory find_files<CR>", -- "find_files"|"grep_string"|"live_grep"
-			desc = "telescope select directory for Find Files",
+			desc = "select directory for Find Files",
 		},
 	},
 }
