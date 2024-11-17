061ee891ea4cab326b4343eebfa584af85cf93e6
Kira Sokolova <Kyra256@proton.me>
2024-11-17 05:12:01 +0300

    fix(quarto): stop opening output in floating window

--- configs/filetype/documents/init.lua
+++ configs/filetype/documents/init.lua
@@ -100,11 +100,10 @@ M.jupytext = {
 function M.molten()
 	vim.g.molten_image_provider = "image.nvim"
 	vim.g.molten_copy_output = true
+	vim.g.molten_auto_open_output = false
 	vim.g.molten_output_win_max_height = 20
-	vim.g.molten_auto_open_output = true
 	vim.g.molten_wrap_output = true
 	vim.g.molten_virt_text_output = true
-	vim.g.molten_virt_lines_off_by_1 = true
 end
 
 function M.quarto(_, _)
